c9a944dae82421c8432e11909d3b233fb2753d5c
SYSTEMS\saira.ahmed <saira.ahmed@systemsltd.com>
2023-09-08 15:40:39 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import './App.css';
+import 'bootstrap';
+import Header from './components/Layout/Header';
+import "bootstrap/dist/css/bootstrap.min.css"
+import Footer from './components/Layout/Footer';
+import SignUp from './components/Forms/SignUp';
+import SignIn from './components/Forms/SignIn';
+import { useState } from 'react';
+import Blogs from './components/Blogs/Blogs';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import Dashboard from './components/Dashboard/Dashboard';
+import TransferForm from './components/Forms/TransferForm';
+
+
+function App() {
+
+  const navigate = useNavigate();
+  const [users, setUsers] = useState(
+    [{
+      name: "Saira Bano",
+      email: "noonarisaira@gmail.com",
+      password: "abcd",
+      failedAttempts: 0
+    }]
+  );
+
+  const [userLoginFlag, setUserLoginFlag] = useState(false);
+
+
+
+
+  const registerUser = (userData) => {
+    setUsers((prevUsers) => {
+      return [...prevUsers, userData]
+    });
+    navigate('sign-in');
+  }
+
+
+  const failedAttempt = (failedUser) => {
+    setUsers((prevUsers) => {
+      return prevUsers.map(user => {
+        if (user.email === failedUser.email) {
+          return failedUser;
+        }
+        return user;
+      })
+    })
+  }
+
+  return (
+
+      < div className="App">
+        <Header isUserLoggedIn={userLoginFlag} />
+        <Routes>
+          <Route path='dashboard' element={<Dashboard />} />
+          <Route path='transfer' element={<TransferForm />} />
+          <Route path='blogs' element={<Blogs />} />
+          <Route path='/' element={<SignUp registerUser={registerUser} />} />
+          <Route path='sign-in' element={
+            <SignIn users={users} failedAttempt={failedAttempt} setUserLoginFlag={setUserLoginFlag} />
+          } />
+        </Routes>
+        <Footer />
+      </div>
+
+  );
+}
+
+export default App;
